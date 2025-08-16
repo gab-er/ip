6bcc3f49225d5405755dd49953fc4b977468f971
Gabriel Er <gabrielerkianrong@gmail.com>
2025-08-16 21:08:53 +0800

    Add echo and exit on bye

--- src/main/java/Jimmy.java
+++ src/main/java/Jimmy.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 public class Jimmy {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        // Initialise variables
+        Scanner scan = new Scanner(System.in);
         String chatBotName = "Jimmy";
-        System.out.println("_________________________________________________");
+        String horizontalDivider = "_________________________________________________";
+
+        // Greetings
+        System.out.println(horizontalDivider);
         System.out.println("Hello! I'm " + chatBotName);
         System.out.println("What can I do for you?");
-        System.out.println("_________________________________________________");
+        System.out.println(horizontalDivider + "\n"); // Add newline so user input is on next line
+
+        // Retrieve user input
+        String command = scan.next();
+
+        // Only exit if "bye" is inputted
+        while (!command.equals("bye")) {
+            // Echo
+            System.out.println(horizontalDivider);
+            System.out.println(command);
+            System.out.println(horizontalDivider);
+            command = scan.next();
+        }
+
+        // Exit
+        System.out.println(horizontalDivider);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalDivider);
     }
 }
