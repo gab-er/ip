6a342c03fca2a54fb2b162220895c24b21712a62
Gabriel Er <gabrielerkianrong@gmail.com>
2025-08-16 21:27:53 +0800

    Add ability to store text and display stored text

--- src/main/java/Jimmy.java
+++ src/main/java/Jimmy.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jimmy {
     public static void main(String[] args) {
@@ -6,6 +7,7 @@ public class Jimmy {
         Scanner scan = new Scanner(System.in);
         String chatBotName = "Jimmy";
         String horizontalDivider = "_________________________________________________";
+        ArrayList<String> storedText = new ArrayList<>(); // Array to store commands given
 
         // Greetings
         System.out.println(horizontalDivider);
@@ -14,15 +16,24 @@ public class Jimmy {
         System.out.println(horizontalDivider + "\n"); // Add newline so user input is on next line
 
         // Retrieve user input
-        String command = scan.next();
+        String command = scan.nextLine();
 
         // Only exit if "bye" is inputted
         while (!command.equals("bye")) {
-            // Echo
-            System.out.println(horizontalDivider);
-            System.out.println(command);
-            System.out.println(horizontalDivider);
-            command = scan.next();
+            // Display stored text
+            if (command.equals("list")) {
+                for (int i = 0; i < storedText.size(); i++) {
+                    int id = i + 1; // Text ID starting from 1
+                    System.out.println(id + ". " + storedText.get(i));
+                }
+            } else {
+                // Echo
+                storedText.add(command); // Store entered text
+                System.out.println(horizontalDivider);
+                System.out.println("added:" + command);
+                System.out.println(horizontalDivider);
+            }
+            command = scan.nextLine();
         }
 
         // Exit
