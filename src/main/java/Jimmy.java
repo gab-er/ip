a7a45a13a9f029f47f678753e69d0f491ef3bd97
Gabriel Er <gabrielerkianrong@gmail.com>
2025-08-16 20:46:56 +0800

    Rename and add greet, exit

---
+++ src/main/java/Jimmy.java
@@ -0,0 +1,15 @@
+public class Jimmy {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String chatBotName = "Jimmy";
+        System.out.println("_________________________________________________");
+        System.out.println("Hello! I'm " + chatBotName);
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
