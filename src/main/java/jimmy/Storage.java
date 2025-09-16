3b7b089742da63eed952e223e7c48555f149d66d
gab-er <gabrielerkianrong@gmail.com>
2025-09-16 17:29:08 +0800

    Improve code quality

--- src/main/java/jimmy/Storage.java
+++ src/main/java/jimmy/Storage.java
@@ -78,51 +78,88 @@ public class Storage {
      * @return appropriate Task depending on the line entry.
      */
     public Task readData(String dataEntry) throws JimmyException {
+        String[] parsedData = dataEntry.split(DELIMITER);
+        String taskType = parsedData[1];
+
+        switch (taskType.toLowerCase()) {
+        case ("todo"):
+            assert parsedData.length == TODO_FIELD_COUNT;
+            return createToDo(parsedData);
+        case ("deadline"):
+            assert parsedData.length == DEADLINE_FIELD_COUNT;
+            return createDeadline(parsedData);
+        case ("event"):
+            assert parsedData.length == EVENT_FIELD_COUNT;
+            return createEvent(parsedData);
+        default:
+            throw new JimmyException("Error in reading data");
+        }
+    }
+
+    /**
+     * Returns a new ToDo task with appropriate information based on the parsed data.
+     * @param parsedData Array of strings containing the parsed data
+     * @return ToDo task with appropriate information based on the parsed data.
+     */
+    public ToDo createToDo(String[] parsedData) {
+        String tag;
+        String description;
+        String isCompletedString;
+
+        // Format: TAG|TODO|DESCRIPTION|COMPLETED
+        tag = parsedData[0];
+        description = parsedData[2];
+        isCompletedString = parsedData[3];
+
+        ToDo newTodo = new ToDo(description, isCompletedString.equalsIgnoreCase("true"), tag);
+        return newTodo;
+    }
+
+    /**
+     * Returns a new Deadline task with appropriate information based on the parsed data.
+     * @param parsedData Array of strings containing the parsed data
+     * @return Deadline task with appropriate information based on the parsed data.
+     */
+    public Deadline createDeadline(String[] parsedData) throws JimmyException {
         String tag;
         String description;
         String isCompletedString;
         String deadline;
+
+        // Format: TAG|DEADLINE|DESCRIPTION|COMPLETED|DEADLINE
+        tag = parsedData[0];
+        description = parsedData[2];
+        isCompletedString = parsedData[3];
+        deadline = parsedData[4];
+
+        Deadline newDeadline = new Deadline(description, isCompletedString.equalsIgnoreCase("true"),
+                tag, deadline);
+
+        return newDeadline;
+    }
+
+    /**
+     * Returns a new Event task with appropriate information based on the parsed data.
+     * @param parsedData Array of strings containing the parsed data
+     * @return Event task with appropriate information based on the parsed data.
+     */
+    public Event createEvent(String[] parsedData) throws JimmyException {
+        String tag;
+        String description;
+        String isCompletedString;
         String start;
         String end;
 
-        String[] parsedData = dataEntry.split(DELIMITER);
-        String taskType = parsedData[1];
-        switch (taskType.toLowerCase()) {
-        case ("todo"):
-            // Format: TAG|TODO|DESCRIPTION|COMPLETED
-            assert parsedData.length == TODO_FIELD_COUNT;
-            tag = parsedData[0];
-            description = parsedData[2];
-            isCompletedString = parsedData[3];
+        // Format: TAG|EVENT|DESCRIPTION|COMPLETED|START|END|
+        tag = parsedData[0];
+        description = parsedData[2];
+        isCompletedString = parsedData[3];
+        start = parsedData[4];
+        end = parsedData[5];
 
-            ToDo newTodo = new ToDo(description, isCompletedString.equalsIgnoreCase("true"), tag);
-            return newTodo;
-        case ("deadline"):
-            // Format: TAG|DEADLINE|DESCRIPTION|COMPLETED|DEADLINE
-            assert parsedData.length == DEADLINE_FIELD_COUNT;
-            tag = parsedData[0];
-            description = parsedData[2];
-            isCompletedString = parsedData[3];
-            deadline = parsedData[4];
-
-            Deadline newDeadline = new Deadline(description, isCompletedString.equalsIgnoreCase("true"),
-                    tag, deadline);
-            return newDeadline;
-        case ("event"):
-            // Format: TAG|EVENT|DESCRIPTION|COMPLETED|START|END|
-            assert parsedData.length == EVENT_FIELD_COUNT;
-            tag = parsedData[0];
-            description = parsedData[2];
-            isCompletedString = parsedData[3];
-            start = parsedData[4];
-            end = parsedData[5];
-
-            Event newEvent = new Event(description, isCompletedString.equalsIgnoreCase("true"), tag,
-                    start, end);
-            return newEvent;
-        default:
-            throw new JimmyException("Error in reading data");
-        }
+        Event newEvent = new Event(description, isCompletedString.equalsIgnoreCase("true"), tag,
+                start, end);
+        return newEvent;
     }
 
     /**
